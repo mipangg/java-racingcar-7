bb1677db320425e1cb085d66e8c9d1b1b47e7283
mipangg <morganjeong119@gmail.com>
2025-06-27 12:22:13 +0900

    feat(Car): 각 자동차에 5자 이하의 이름을 부여할 수 있다

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,4 +1,9 @@
 package racingcar;
 
 public class Car {
+    String name;
+
+    public Car(String name) {
+        this.name = name;
+    }
 }
--- src/test/java/racingcar/CarTests.java
+++ src/test/java/racingcar/CarTests.java
@@ -1,4 +1,18 @@
+package racingcar;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 class CarTests {
-  
+
+    @Test
+    @DisplayName("각 자동차에 5자 이하의 이름을 부여할 수 있다.")
+    void car_have_name_under_5() throws Exception {
+        Car car = new Car("car1");
+
+        assertEquals("car1", car.name);
+    }
+
 }
