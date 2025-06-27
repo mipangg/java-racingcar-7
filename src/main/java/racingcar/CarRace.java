99533ae44f5accaf11eadb6d2478fb1fb181174b
mipangg <morganjeong119@gmail.com>
2025-06-27 18:46:51 +0900

    feat(CarRace): 자동차 경주 게임을 완료한 후 우승자를 선별할 수 있다.

--- src/main/java/racingcar/CarRace.java
+++ src/main/java/racingcar/CarRace.java
@@ -4,12 +4,32 @@ import java.util.List;
 
 public class CarRace {
 
-    void race(List<Car> cars, int times) {
+    List<Car> cars;
+
+    public CarRace(List<Car> cars) {
+        this.cars = cars;
+    }
+
+    public void race(int times) {
         for (int i = 0; i < times; i++) {
             for (Car car : cars) {
                 car.move();
             }
         }
+    }
+
+    public Car getWinner() {
+        Car winner = null;
+        for (Car car : cars) {
+            if (winner == null) {
+                winner = car;
+                continue;
+            }
+            if (car.getMove() > winner.getMove()) {
+                winner = car;
+            }
+        }
+        return winner;
     }
 
 }
--- src/test/java/racingcar/CarRaceTests.java
+++ src/test/java/racingcar/CarRaceTests.java
@@ -2,6 +2,7 @@ package racingcar;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static racingcar.TestUtils.genCarList;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,20 +13,26 @@ import racingcar.util.DefaultRandomGenerator;
 
 class CarRaceTests {
 
-    CarRace carRace = new CarRace();
+    List<Car> cars = genCarList();
+    CarRace carRace = new CarRace(cars);
 
     @Test
     @DisplayName("주어진 횟수 동안 n대의 자동차는 경주한다. ")
     void cars_run_for_given_times() throws Exception {
 
-        Car car1 = new Car("car1", () -> 5);
-        Car car2 = new Car("car2", () -> 1);
-        List<Car> cars = List.of(car1, car2);
+        carRace.race( 3);
+        assertThat(cars.get(0).getMove()).isEqualTo(3);
+        assertThat(cars.get(1).getMove()).isEqualTo(0);
 
-        carRace.race(cars, 3);
+    }
 
-        assertThat(car1.getMove()).isEqualTo(3);
-        assertThat(car2.getMove()).isEqualTo(0);
+    @Test
+    @DisplayName("자동차 경주 게임을 완료한 후 우승자를 선별할 수 있다. ")
+    void winner_exist_after_game() throws Exception {
+
+        Car expectedWinner = cars.get(0);
+        carRace.race(3);
+        assertThat(carRace.getWinner()).isEqualTo(expectedWinner);
 
     }
     
